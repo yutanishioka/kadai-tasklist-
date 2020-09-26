1f4a48d6b5709e7735e73310f2bd92de82202c3a
yuta.nishioka <yuta914.makenew@gmail.com>
2020-09-26 05:17:16 +0000

    saiteisyutsu

--- db/migrate/20200921092404_add_status_to_tasks.rb
+++ db/migrate/20200921092404_add_status_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddStatusToTasks < ActiveRecord::Migration[5.2]
   def change
     add_column :tasks, :status, :string
-    add_column :tasks, :string, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_092404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    t.string "string"
   end
 
 end
