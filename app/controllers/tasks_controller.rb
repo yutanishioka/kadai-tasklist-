498ece8d23017da2cdaaa193e7bf0d6422ed81f8
yuta.nishioka <yuta914.makenew@gmail.com>
2020-10-03 06:54:14 +0000

    tasklist login2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find(params[:id])
     end
     
     # Strong Parameter
