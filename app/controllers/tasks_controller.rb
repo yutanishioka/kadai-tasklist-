b5ef7120e93bb3a88556f279bd481bf6016ffe13
yuta.nishioka <yuta914.makenew@gmail.com>
2020-10-06 00:50:17 +0000

    tasklist login4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,7 @@ class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     
     def index
-        
-            @task = current_user.tasks.build  # form_with 用
-            @task = current_user.tasks.order(id: :desc).page(params[:page])
-
+        @task = current_user.tasks.order(id: :desc).page(params[:page])
     end
     
     def show
